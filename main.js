1b316a30cf54ee343ef6971b66e72bffb53b554f
alana <aw@oerli.net>
2024-09-04 12:10:11 +0200

    test

---
+++ main.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById("canvas");
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "red";
+ctx.fillRect(100, 100, 100, 100);
